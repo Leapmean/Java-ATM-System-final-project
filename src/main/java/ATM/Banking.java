202ef1bb9366f8824859e204bebe4333b4c62865
leapmean <hengleapmean5@gmail.com>
2026-09-18 18:33:20 +0700

    feat: add findAccount method to Banking.java.

--- src/main/java/ATM/Banking.java
+++ src/main/java/ATM/Banking.java
@@ -1,4 +1,18 @@
 package ATM;
+import java.util.ArrayList;
 
 public class Banking {
+
+    ArrayList<Account> accounts = new ArrayList<Account>();
+    int accountCount = 1;
+    String historyFile = "History.txt";
+
+    Account findAccount(String accountId) throws AccountNotFoundException{
+        for(int i = 0; i < accounts.size(); i++){
+            if(accounts.get(i).getAccountId().equals(accountId)) {
+                return accounts.get(i);
+            }
+        }
+        throw new AccountNotFoundException("Account is not found!");
+    }
 }
