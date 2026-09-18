d5183df30974833a6b79bb98bea9ed803182f6a2
leapmean <hengleapmean5@gmail.com>
2026-09-18 17:22:53 +0700

    feat: declare varieble and implement contructor

--- src/main/java/ATM/Account.java
+++ src/main/java/ATM/Account.java
@@ -3,22 +3,18 @@ package ATM;
 import java.util.Scanner;
 
 public class Account {
-    public static void main (String[] args){
+    private String accountId;
+    private String name;
+    private String password;
+    private double balance;
+    protected double dailyLimit;
+    private double todayWithdraw;
 
-        Banking bank = new Banking();
-        Scanner input = new Scanner(System.in);
-        boolean bankingFunction = true;
-
-        while(bankingFunction){
-            System.out.println("===========ATM==========");
-            System.out.println("1. Check balance");
-            System.out.println("2. Deposit");
-            System.out.println("3. Withdraw");
-            System.out.println("4. Transfer");
-            System.out.println("5. Show transaction history");
-            System.out.println("6. Exit");
-            System.out.println("Pick an option: ");
-            int choice = Integer.parseInt(input.nextLine());
-        }
+    Account(String accountId, String name, String password, double balance, double todayWithdraw){
+        this.accountId = accountId;
+        this.name = name;
+        this.password = password;
+        this.balance = balance;
+        this.todayWithdraw = todayWithdraw;
     }
 }
