066e0108cee4b6c528480e0e593e6e7014ca1041
leapmean <hengleapmean5@gmail.com>
2026-09-18 21:32:13 +0700

    feat: declare variable implement constructor and override two method "withdraw and getAccountType" into CheckingAccount.java.

--- src/main/java/ATM/CheckingAccount.java
+++ src/main/java/ATM/CheckingAccount.java
@@ -1,4 +1,23 @@
 package ATM;
 
-public class CheckingAccount {
+public class CheckingAccount extends Account {
+    private double overDraftLimit = 50;
+    public CheckingAccount(String accountId, String name, String password, double balance){
+        super(accountId, name, password, balance, 1000);
+    }
+    @Override
+    public Transaction withdraw(double amount)
+            throws InvalidAmountException, InsufficientFundsException, DailyLimitExceededException{
+        checkBasicWithdrawRule(amount);
+        if(getBalance() - amount < -overDraftLimit){
+            throw new InsufficientFundsException("That would go over your $ " + overDraftLimit + " overdaraft limit.");
+        }
+        reduceBalance(amount);
+        addToWithdrawToday(amount);
+        return new Transaction("Withdraw", amount, getBalance());
+    }
+    @Override
+    public String getAccountType(){
+        return "Checking";
+    }
 }
