b578506b7401740797412018a8bc9f9208971b16
leapmean <hengleapmean5@gmail.com>
2026-09-18 20:13:19 +0700

    feat: declare variable implement contructor and override two method "withdraw and getAccountType".

--- src/main/java/ATM/SavingAccount.java
+++ src/main/java/ATM/SavingAccount.java
@@ -1,4 +1,26 @@
 package ATM;
 
-public class SavingAccount {
+public class SavingAccount extends Account {
+    private double minimumBalance = 0;
+    public SavingAccount(String accountId, String name, String password, double balance){
+        super(accountId, name,password, balance, 0);
+    }
+
+    @Override
+    public Transaction withdraw(double amount)
+            throws InvalidAmountException, InsufficientFundsException, DailyLimitExceededException{
+
+        checkBasicWithdrawRule(amount);
+        if (getBalance() - amount < minimumBalance) {
+            throw new InsufficientFundsException("Saving account must keep atleast $" + minimumBalance);
+        }
+        reduceBalance(amount);
+        addToWithdrawToday(amount);
+        return new Transaction("Withdraw" ,amount ,getBalance());
+    }
+
+    @Override
+    public String getAccountType(){
+        return "Saving Account.";
+    }
 }
