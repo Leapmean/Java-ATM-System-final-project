8f4a4d2092b52e952fddad200b97b937d334333d
Kaknika9 <kaknika189999@icloud.com>
2026-09-20 18:12:00 +0700

    Add AuthService for login, PIN change and unlock

---
+++ src/main/java/org/example/service/AuthService.java
@@ -0,0 +1,48 @@
+package org.example.service;
+import org.example.exception.AccountLockedException;
+import org.example.exception.AccountNotFoundException;
+import org.example.exception.InvalidPinException;
+import org.example.model.Card;
+import java.util.HashMap;
+import java.util.Map;
+
+public class AuthService {
+    private Map<String, Card> cards = new HashMap<>();
+    public void addCard(Card card){
+        cards.put(card.getCardNumber(), card);
+    }
+    public Card login(String cardNumber, String pin) throws AccountNotFoundException, AccountLockedException,InvalidPinException{
+        Card card= cards.get(cardNumber);
+        if (card == null){
+            throw new AccountNotFoundException("This Card Number not found.");
+        }
+        if (card.isLocked()){
+            throw new AccountLockedException("Your Card is Locked.");
+        }
+        if (!card.checkPin(pin)){
+            card.addFailedAttempt();
+            if (card.isLocked()){
+                throw new AccountLockedException("Too many wrong attempts. Your card is Locked.");
+            }
+            throw new InvalidPinException("Wrong PIN. Please try again");
+        }
+        card.resetFailedAttempts();
+        return card;
+    }
+    public void changePin(Card card, String oldPin, String newPin)throws InvalidPinException{
+        if (!card.checkPin(oldPin)){
+            throw new InvalidPinException("Old PIN is incorrect.");
+        }
+        if (newPin.length() !=4){
+            throw new InvalidPinException("New PIN must be 4 digits.");
+        }
+        card.setPin(newPin);
+    }
+    public void unlockCard(String cardNumber) throws AccountNotFoundException{
+        Card card = cards.get(cardNumber);
+        if (card == null){
+            throw new AccountNotFoundException("Card number not found.");
+        }
+        card.unlock();
+    }
+}
