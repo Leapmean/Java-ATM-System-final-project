f198bec3e77de2dd93d83cad17e48c0cfaf71edf
Kaknika9 <kaknika189999@icloud.com>
2026-09-20 16:56:37 +0700

    feat: add package org.example.model than import java

---
+++ src/main/java/org/example/model/Card.java
@@ -0,0 +1,9 @@
+package org.example.model;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Base64;
+
+public class Card {
+}
