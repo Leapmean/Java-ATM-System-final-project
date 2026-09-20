f8e2c2b46e385389ebc1590955a1e0a611269ae1
Kaknika9 <kaknika189999@icloud.com>
2026-09-20 17:35:45 +0700

    Add Card class

--- src/main/java/org/example/model/Card.java
+++ src/main/java/org/example/model/Card.java
@@ -1,9 +1,49 @@
 package org.example.model;
-import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-import java.util.Base64;
 
 public class Card {
+    private String cardNumber;
+    private String accountNumber;
+    private String pin;
+    private int failedAttempts;
+    private boolean locked;
+
+    public Card(String cardNumber, String accountNumber, String pin){
+        this.cardNumber = cardNumber;
+        this.accountNumber = accountNumber;
+        this.pin = pin;
+        this.failedAttempts = 0;
+        this.locked = false;
+    }
+
+    public boolean checkPin(String enteredPin){
+        return pin.equals(enteredPin);
+    }
+    public void setPin (String newPin){
+        this.pin = newPin;
+    }
+    private void addFailedAttempts(){
+        failedAttempts++;
+        if (failedAttempts>=3){
+            locked = true;
+        }
+    }
+    public void resetFailedAttempts(){
+        failedAttempts = 0;
+    }
+    public void unlock(){
+        locked = false;
+        failedAttempts = 0;
+    }
+    public String getCardNumber(){
+        return cardNumber;
+    }
+    public String getAccountNumber(){
+        return accountNumber;
+    }
+    public int getFailedAttempts(){
+        return failedAttempts;
+    }
+    public boolean isLocked(){
+        return locked;
+    }
 }
